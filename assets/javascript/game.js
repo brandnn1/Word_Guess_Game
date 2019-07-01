49d017ccbe8942e6dc2f3dadd0f91e60ac7d62c2
brandnn <brandnn@uw.edu>
2019-07-01 12:29:27 -0700

    Added guesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,11 @@
     var wins = 0;
     var losses = 0;
-    var ties = 0;
+    var guesses = 15;
     var directionsText = document.getElementById ("directions-text");
     var userChoiceText = document.getElementById ("userchoice-text");
-    var computerChoiceText = document.getElementById ("computerchoice-text");
     var winsText = document.getElementById ("wins-text");
+    var guessesText = document.getElementById ("guesses-text");
     var lossesText = document.getElementById ("losses-text");
-    var tiesText = document.getElementById ("ties-text");
-    var avgText = document.getElementById ("winlossavg-text");
 
 
     document.onkeypress = function(event) 
@@ -15,3 +13,30 @@
     
       // Determines which key was pressed.
       var userGuess = event.key;
+        
+      if(userGuess === 'r' || userGuess === 'p' || userGuess === 's')
+      {
+      userChoiceText.textContent = "You chose: " + userGuess.toUpperCase();
+      computerChoiceText.textContent = "Computer Chose: " + computerGuess.toUpperCase();
+      winsText.textContent = "Total Wins: " + wins;
+      lossesText.textContent = "Total Losses: " + losses;
+      tiesText.textContent = "Total Ties: " + ties;
+      avgText.textContent = "Your Win Average is: " + avg1 + "%";
+      }
+
+      if ((userGuess == "s") && (computerGuess == "p") ||
+         (userGuess == "r") && (computerGuess == "s") ||
+         (userGuess == "p") && (computerGuess == "r")) 
+      {
+      wins++; 
+      }
+        else if (userGuess === computerGuess) 
+        {
+          ties++;
+        }
+      else 
+      {
+        losses++;
+      }
+
+    };
